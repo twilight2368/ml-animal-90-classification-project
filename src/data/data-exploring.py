aa6a4fd4ed92d5473892c403bdac0d10bd053da3
Anonymous <103519498+twilight2368@users.noreply.github.com>
2024-10-18 07:17:56 +0700

    first commit

---
+++ src/data/data-exploring.py
@@ -0,0 +1,25 @@
+import os
+
+
+def data_exploring(PATH_DIR):
+    os.chdir(PATH_DIR)
+
+    for folder in os.listdir():
+        print(folder)
+
+    labels = []
+
+    for root, dirs, files in os.walk(PATH_DIR):
+        print(f"Current directory: {root}")
+        label = root.split("/")[-1]
+        labels.append(label)
+        labels.sort()
+    print("--------------------------------")
+    if (len(labels)):
+        labels.pop(0)
+        for label in labels:
+            print(label, end=",")
+
+    print(f"Number of labels:{len(labels)} ")
+
+    return
